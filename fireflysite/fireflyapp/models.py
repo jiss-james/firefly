0d671cef88bc3d2ffafaa8c70033cdfacad705b6
Pavel Vasilev <pavel.vasilev@mycit.ie>
2018-05-17 12:24:12 +0100

    Update Dark Sky model

--- fireflysite/fireflyapp/models.py
+++ fireflysite/fireflyapp/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+# Create your models here.// What else do we need here ?
+class DarkSky(models.Model):
+    long = models.CharField(max_length=30)
+    lat = models.CharField(max_length=30)
+    temperature = models.CharField(max_length=30)
+    humidity = models.CharField(max_length=30)
+    timestamp = models.CharField(max_length=30)
+    windBearing = models.CharField(max_length=30)
+    windGust = models.CharField(max_length=30)
+    windSpeed = models.CharField(max_length=30)
