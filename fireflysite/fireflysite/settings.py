b87c7febe9e126badcab14a4b752d40fb7053d3c
Jiss James <jissjames360@gmail.com>
2018-05-31 12:24:56 +0100

    updated requirements.txt

--- fireflysite/fireflysite/settings.py
+++ fireflysite/fireflysite/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/2.0/ref/settings/
 """
 
 import os
+import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -134,3 +135,6 @@ EMAIL_FILE_PATH = os.path.join(BASE_DIR, "sent_emails")
 # after login and logout go back to index
 LOGIN_REDIRECT_URL = '/'
 LOGOUT_REDIRECT_URL = '/'
+
+# Activate Django-Heroku.
+django_heroku.settings(locals())
